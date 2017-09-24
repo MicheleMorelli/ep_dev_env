c41e6661db0469b12de5bf7ee209d880fac7dbe9
MicheleMorelli <morelli.michele@hotmail.it>
2017-09-24 22:43:32 +0000

    adding templates

---
+++ templates/10_core_test.pl
@@ -0,0 +1,9 @@
+# This file was created by bin/epadmin
+# You can regenerate this file by doing ./bin/epadmin config_core template
+$c->{host} = 'localhost';
+$c->{port} = 8080;
+$c->{aliases} = [];
+$c->{securehost} = $c->{host};
+$c->{securehost} = '';
+$c->{secureport} = 443;
+$c->{http_root} = undef;
---
+++ templates/SystemSettings.pm
@@ -0,0 +1,11 @@
+package EPrints::SystemSettings;
+
+$EPrints::SystemSettings::conf = 
+{
+	'user' => 'vagrant',
+	'group' => 'vagrant',
+	'smtp_server' => 'localhost',
+	'load_defaultcfg' => 1,
+};
+
+1;
